2736f0d4bb78393dd31a9ac397df07c9fa51786b
Pedro Madureira <ph.websolucoes@gmail.com>
2024-02-21 11:12:25 -0300

    flutter web build added

--- fully_featured/urls.py
+++ fully_featured/urls.py
@@ -1,9 +1,30 @@
 from django.urls import path, include
 from django.contrib import admin
+from django.views.static import serve
+import os
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+FLUTTER_WEB_APP = os.path.join(BASE_DIR, 'flutter_web_app')
+
+def flutter_redirect(request, resource):
+    return serve(request, resource, FLUTTER_WEB_APP)
+
+flutter_app_routes = [
+    path("login", lambda r: flutter_redirect(r, 'index.html')),
+    path("profile", lambda r: flutter_redirect(r, 'index.html')),
+    path("menu", lambda r: flutter_redirect(r, 'index.html')),
+    path("todo", lambda r: flutter_redirect(r, 'index.html')),
+    path("journal", lambda r: flutter_redirect(r, 'index.html')),
+    path("note", lambda r: flutter_redirect(r, 'index.html')),
+    path("glossary", lambda r: flutter_redirect(r, 'index.html')),
+]
 
 urlpatterns = [
-    path("", include("fully_featured.core.urls")),
-    path("user/", include("fully_featured.user.urls")),
+    path("", lambda r: flutter_redirect(r, 'index.html')),
+    *flutter_app_routes,
+    path("api/", include("fully_featured.core.urls")),
+    path("api/user/", include("fully_featured.user.urls")),
     path("admin/", admin.site.urls),
+    path("<path:resource>", flutter_redirect),
 ]
